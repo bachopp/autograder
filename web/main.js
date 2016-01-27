552a3651d121d26cdcd3319225d89177ebc2bc9b
tokams <t.gliniecki@hotmail.com>
2016-01-27 15:28:22 +0100

    go server testing gorilla

--- web/main.js
+++ web/main.js
@@ -1,8 +1,22 @@
-// main.js
-var React = require('react');
-var ReactDOM = require('react-dom');
+// // main.js
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+//
+// ReactDOM.render(
+//   <h1>Welcome to Autograder</h1>,
+//   document.getElementById('example')
+// );
 
-ReactDOM.render(
-  <h1>Welcome to Autograder</h1>,
-  document.getElementById('example')
-);
+
+var ws = new WebSocket("ws://localhost:8004/ws");
+
+function foo(response) {
+  var data = response.data;
+  ws.send(data.login)
+  ws.send(data.name)
+}
+
+var script = document.createElement('script');
+script.src = 'https://api.github.com/users/tokams?callback=foo';
+
+document.getElementsByTagName('head')[0].appendChild(script);
