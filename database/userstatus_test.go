d91d8479b6aa8cb9e61c1560e4b070365ae6f80f
Tomasz Gliniecki <t.gliniecki@hotmail.com>
2016-04-07 14:28:13 +0200

    added more courses to teacher

--- database/userstatus_test.go
+++ database/userstatus_test.go
@@ -11,19 +11,19 @@ func TestUser(t *testing.T) {
 	ida := 1
 	AddCourse("DAT200")
 	idb := 2
-	// AddCourse("DAT210")
-	// idc := 3
+	AddCourse("DAT210")
+	idc := 3
 	AddCourse("DAT220")
 	ide := 3
-	// AddCourse("DAT310")
-	// idg := 6
+	AddCourse("DAT310")
+	idg := 6
 	AddCourse("DAT320")
 	idh := 4
 	AddCourse("DAT230")
 	idf := 5
 
 	a := Role{"admin", []courses{{idf, "DAT230"}}}
-	b := Role{"teacher", []courses{{ida, "DAT100"}, {idb, "DAT200"}}}
+	b := Role{"teacher", []courses{{ida, "DAT100"}, {idb, "DAT200"}, {idc, "DAT210"}, {idg, "DAT310"}}}
 	c := Role{"student", []courses{{idh, "DAT320"}, {ide, "DAT220"}}}
 
 	UpgradeUser("thomas", a, b, c)
